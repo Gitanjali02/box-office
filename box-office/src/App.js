68f34eb8f5a3b01d6cca1bd47040695425d3b7ba
Gitanjali Kumari <your_address_email@domain.com>
2022-07-13 13:31:59 +0530

    layout

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { Switch, Route } from 'react-router-dom';
+//import Navs from './components/Navs'
+import Home from './pages/Home'
+import Starred from './pages/Starred'
 
 function App() {
   return (
+    
+      
     <Switch>
-      <Route  path="/starred">
-        This is home page.
+    <Route exact path="/">
+          <Home />
         </Route>
 
-        <Route path="/">
-          Page is 404 not found
+      <Route  exact path="/starred">
+        <Starred />
+        
         </Route>
+
+        
+
+        <Route>
+          <div>
+            not found
+          </div></Route>
     </Switch>
+    
   );
 }
 
